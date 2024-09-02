6b9d9ce85b468dcb24b21ee7f2d13f35118d523b
LenonFelipe <lenonfelipe98@gmail.com>
2024-09-02 15:54:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let container = document.querySelector('.container')
+let formulario = document.querySelector('.formulario')
+let pesquisa = document.querySelector('.form')
+
+async function getFilmes(buscar){
+    let request = await fetch(`https://www.omdbapi.com/?t=${buscar}&plot=full&apikey=b2332d31`)
+    let data = await request.json()
+    
+    console.log(data);
+
+    container.innerHTML += `
+    <img src="${data['Poster']}" alt="">
+    <h1 style="
+    left: 80px;
+    text-transform: uppercase;
+    ">${data['Title']}</h1>
+    <p><strong>Duração: </strong>${data['Runtime']}</p>
+    <p><strong>Ano: </strong>${data['Year']}</p>
+    <p><strong>Diretor: </strong>${data['Director']}</p>
+    <p><strong>Gênero: </strong>${data['Genre']}</p>
+    <p style="
+    width: 800px;
+    position: relative;
+    left: 380px;
+    top: -370px;"><strong>Sinopse: </strong> ${data['Plot']}</p>
+    `
+
+}
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    container.innerHTML = ''
+    getFilmes(pesquisa.value)
+    pesquisa.value = '' 
+})
